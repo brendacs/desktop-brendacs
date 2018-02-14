4ebc84dc83b74ad3cca11b178903355ca292da45
Brenda Zhang <brenda.zhang2@gmail.com>
2018-02-13 16:52:26 -0800

    Finish migrating to Jekyll structure and base modularization of HTML

---
+++ _site/js/finder.js
@@ -0,0 +1,115 @@
+$(document).ready(function() {
+  // folders
+  $('.finder-directory-container').on('click', function(e) {
+    $('.finder-directory-container').removeClass('highlight');
+    $('.finder-item-container').removeClass('highlight');
+    $(this).addClass('highlight');
+    $('.finder-section.finder-inner-items-list-container').addClass('finder-section-border');
+    $('.finder-inner-items-list').addClass('hide');
+    $('.finder-selected-item-wrapper').addClass('hide');
+  });
+
+  // folder outer directories
+  $('.education-folder').on('click', function(e) {
+    $('.education-items').removeClass('hide');
+  });
+
+  $('.experience-folder').on('click', function(e) {
+    $('.experience-items').removeClass('hide');
+  });
+
+  $('.projects-folder').on('click', function(e) {
+    $('.project-items').removeClass('hide');
+  });
+
+  $('.documents-folder').on('click', function(e) {
+    $('.document-items').removeClass('hide');
+  });
+
+  // folder items
+  $('.finder-item-container').on('click', function(e) {
+    $('.finder-item-container').removeClass('highlight');
+    $(this).addClass('highlight');
+    $('.finder-selected-item-wrapper').addClass('hide');
+  });
+
+  // education
+  $('.finder-item-container.burton-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.burton-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.berkeley-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.berkeley-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.major-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.major-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.minor-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.minor-selected-item').removeClass('hide');
+  });
+
+  // experience
+  $('.finder-item-container.climate-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.climate-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.stowk-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.stowk-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.registria-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.registria-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.aaa-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.aaa-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.decal-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.decal-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.ffl-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.ffl-selected-item').removeClass('hide');
+  });
+
+  // projects
+  $('.finder-item-container.stopbot-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.stopbot-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.devils-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.devils-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.referobot-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.referobot-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.liquefy-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.liquefy-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.news-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.news-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.csch-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.csch-selected-item').removeClass('hide');
+  });
+
+  // documents
+  $('.finder-item-container.resume-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.resume-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.about-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.about-selected-item').removeClass('hide');
+  });
+
+  $('.finder-item-container.quotes-list-item').on('click', function(e) {
+    $('.finder-selected-item-wrapper.quotes-selected-item').removeClass('hide');
+  });
+});
---
+++ _site/js/window.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+  var zindex = 1;
+  var open = 0;
+
+  // universal window functions
+  $('.window').draggable({ containment: '.pc' });
+  $('.window').resizable({ containment: '.pc' });
+
+  $('.window').on('click', '.button-close', function(e) {
+    $(this).closest('.window').addClass('hide');
+    $('.finder-directory-container').removeClass('highlight');
+    $('.finder-item-container').removeClass('highlight');
+    $('.finder-section.finder-inner-items-list-container').removeClass('finder-section-border');
+    $('.project-items').removeClass('disabled-text');
+    $('.finder-inner-items-list').addClass('hide');
+    $('.finder-selected-item-wrapper').addClass('hide');
+  });
+
+  $('.window').on('click', function(e) {
+    open = open + 1;
+    $(this).closest('.window').css('z-index', zindex + open);
+  });
+
+  // taskbar triggers
+  $('.finder-icon').on('click', function(e) {
+    open = open + 1;
+    $('.finder-window').css('z-index', zindex + open);
+    $('.finder-window').removeClass('hide');
+  });
+
+  $('.browser-icon').on('click', function(e) {
+    open = open + 1;
+    $('.browser-window').css('z-index', zindex + open);
+    $('.browser-window').removeClass('hide');
+  });
+
+  $('.sublime-icon').on('click', function(e) {
+    open = open + 1;
+    $('.sublime-window').css('z-index', zindex + open);
+    $('.sublime-window').removeClass('hide');
+  });
+
+  $('.terminal-icon').on('click', function(e) {
+    open = open + 1;
+    $('.terminal-window').css('z-index', zindex + open);
+    $('.terminal-window').removeClass('hide');
+  });
+
+  // desktop folders
+  $('.desktop-education-folder').on('click', function(e) {
+    open = open + 1;
+    $('.finder-window').css('z-index', zindex + open);
+    $('.education-items').removeClass('hide');
+  });
+
+  $('.desktop-experience-folder').on('click', function(e) {
+    open = open + 1;
+    $('.finder-window').css('z-index', zindex + open);
+    $('.experience-items').removeClass('hide');
+  });
+
+  $('.desktop-projects-folder').on('click', function(e) {
+    open = open + 1;
+    $('.finder-window').css('z-index', zindex + open);
+    $('.project-items').removeClass('hide');
+  });
+
+  $('.desktop-documents-folder').on('click', function(e) {
+    open = open + 1;
+    $('.finder-window').css('z-index', zindex + open);
+    $('.document-items').removeClass('hide');
+  });
+});
